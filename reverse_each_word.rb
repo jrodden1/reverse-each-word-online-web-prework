7f5fecff82764b28bead685251159eced0b58b2c
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 23:05:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
   sent_arr = sentence.split " "
-  sent_arr.collect do |i|
+  sent_arr_rev = sent_arr.collect do |i|
     i.reverse
   end
+  sent_arr_rev.join " "  
 end
